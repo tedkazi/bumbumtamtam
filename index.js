50a4513aeaef7b0f4200fb1b5b5f98ac50136445
cesar180 <cesar18@dump.hr>
2018-04-28 14:28:34 +0200

    Implemented bombing for days dump hr

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
+
+const SUPER_SECRET_PASSWORD = 'lovethebomb69';
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
+
+axios.post('days.dump.hr', {
+  password: SUPER_SECRET_PASSWORD
+});
 
 app.get('/', (request, response) => {
   response.send('Boom World!');
